c8638782923cd3524d3b01cfff8b65f9f0025f98
jy_lee <qjarlfls@naver.com>
2022-01-12 15:24:57 +0900

    add

--- src/com/java/s3/ex1/MemberMain.java
+++ src/com/java/s3/ex1/MemberMain.java
@@ -1,12 +1,14 @@
 package com.java.s3.ex1;
 
+import java.util.ArrayList;
+
 public class MemberMain {
 
 	public static void main(String[] args) {
 		//test, 프로그램 실행
 		MemberData memberData = new MemberData();
-		
-		MemberDTO [] members = memberData.init();
+		ArrayList<MemberDTO> arr = new ArrayList<>();
+		arr = memberData.init();
 
 	}
 
